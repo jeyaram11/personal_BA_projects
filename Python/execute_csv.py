6e0e14c2aee528490d0731d8aff9c3f46e579181
jeyaram11 <143695465+jeyaram11@users.noreply.github.com>
2023-09-27 11:09:40 +0100

    Update execute_csv.py
    updated CSV ETL process to upload dataframe into load_table and execute truncate script

--- Python/execute_csv.py
+++ Python/execute_csv.py
@@ -21,10 +21,39 @@ def csv_update(filename):
     df = pd.read_csv(path)
     print(df)
 
-#truncate file
+  # create connection to mysql server
+    engine = mc.mysql_connection()
+    connection = engine.connect()
 
+    # upload to mysql server
+    try:
+        df.to_sql(name='load_' + table_name, con=engine, if_exists='replace', index=False)
+        print('Successfully uploaded data')
+    except Exception as e:
+        print('Failed ' + str(e))
 
+    # add a truncate function takes data from load table and added to main table.
+    # will come in handy when pulling last 30 days worth of data only
+    with open(truncate_scripts + 'truncate_' + table_name + '.sql', 'r') as sql_script:
+        truncate_script = sql_script.read()
+    # with connection as cursor:
+    #     #cursor.execute(sqlalchemy.text(truncate_script).execution_options(autocommit=True),multi)
+    # connection.close()
 
+    #can only execute single statements so using a for loop I executed each statement
+    for statement in truncate_script.split(';'):
+        if len(statement.strip()) > 0:
 
-#truncate and commit to mysql server
-csv_update('emails')
+            connection.execute(sqlalchemy.text(statement + ';').execution_options(autocommit=True))
+
+    print('Successfully executed truncate')
+    connection.close()
+
+    try:
+        full_cmd_arguments = sys.argv
+        user_input = full_cmd_arguments[1]
+
+    except:
+        user_input = input('Please enter filename:')
+
+csv_update(user_input)
